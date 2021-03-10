19b8b8755bca60a0a0b02f64d3d17bcb87801307
maychondouglas <maychondouglas@gmail.com>
2021-03-10 14:24:40 -0300

    Controll Buttons Changed

---
+++ src/js/models/Fence.js
@@ -0,0 +1,12 @@
+export default class Fence{
+  constructor(fence){
+    if(this.geometricForm == 'circle'){
+      this.radius = fence.radius;
+      this.latitude = fence.latitude;
+      this.longitude = fence.longitude;
+    }else{
+
+    }
+    
+  }
+}
