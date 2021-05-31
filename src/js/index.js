95c5caea29ed620905f3cb004dccaf997d85a6f8
maychondouglas <maychondouglas@gmail.com>
2021-05-30 22:21:11 -0300

    sw.js created

--- src/js/index.js
+++ src/js/index.js
@@ -722,3 +722,11 @@ const inicializarApp = () => {
 
 //Aguarda a página ser iniciada e chama o Início da Aplicação
 window.onload = inicializarApp;
+
+// Service Worker to PWA
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then((reg) => console.log('service worker registered', reg))
+    .catch((err) => console.log('service worker not register', err));
+}
